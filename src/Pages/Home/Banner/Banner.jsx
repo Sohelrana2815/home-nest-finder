54cdba4007e9c78e769fc4ca536cacbb7676092e
Sohelrana2815 <sohel152302@gmail.com>
2024-08-02 06:57:37 +0600

    banner section responsive for all devices

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,22 +3,22 @@ import { Carousel } from "react-responsive-carousel";
 import banner1 from "../../../assets/Banner-img/banner1.jpg";
 import banner2 from "../../../assets/Banner-img/banner2.jpg";
 import banner3 from "../../../assets/Banner-img/banner3.jpg";
-import { FaArrowRight, FaBeer } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 const Banner = () => {
   return (
     <>
-      <div className="border  flex p-4">
-        <div className="border w-3/4">
-          <h2 className="text-7xl font-bold leading-snug">
+      <div className="border flex flex-col md:flex-row p-4">
+        <div className="border w-full md:w-3/4">
+          <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold leading-snug">
             Discover your dream <br /> home with us
           </h2>
         </div>
-        <div className="border w-3/12 mt-10 ">
+        <div className="border w-full md:w-3/12 mt-4 md:mt-10">
           <p className="text-gray-500">
             Embark on a journey through the expansive world of real estate,
             where opportunities unfold in a myriad of possibilities.
           </p>
-          <p className="mt-4 text-black font-bold ">
+          <p className="mt-4 text-black font-bold">
             View all properties
             <span className="btn btn-xs border btn-outline border-black btn-circle ml-3">
               <FaArrowRight />
@@ -29,12 +29,21 @@ const Banner = () => {
       <Carousel>
         <div>
           <img src={banner1} className="rounded-2xl" />
+          <div className="relative">
+            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 container h-10 w-20 py-1 rounded-full mx-auto border bg-slate-800"></div>
+          </div>
         </div>
         <div>
           <img src={banner2} className="rounded-2xl" />
+          <div className="relative">
+            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 container h-10 w-20 py-1 rounded-full mx-auto border bg-slate-800"></div>
+          </div>
         </div>
         <div>
           <img src={banner3} className="rounded-2xl" />
+          <div className="relative">
+            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 container h-10 w-20 py-1 rounded-full mx-auto border-none bg-slate-800"></div>
+          </div>
         </div>
       </Carousel>
     </>
